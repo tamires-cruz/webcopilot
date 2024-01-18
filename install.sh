57780fad59fa36c52a730d02ec6a169b0e82216a
tamires-cruz <69864264+tamires-cruz@users.noreply.github.com>
2024-01-18 09:27:17 -0300

    Update install.sh
    instalando 
    katana
    paramspider
    
    ** retirei amass

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ cd && git clone https://github.com/sqlmapproject/sqlmap.git ~/tools/sqlmap/ 2> /
 cd && git clone https://github.com/ameenmaali/urldedupe.git ~/tools/urldedupe && cd ~/tools/urldedupe && cmake CMakeLists.txt && make && mv urldedupe /usr/bin/ 2> /dev/null
 cd && git clone https://github.com/devanshbatham/OpenRedireX.git ~/tools/OpenRedireX && cd ~/tools/OpenRedireX && sudo pip3 install -r requirements.txt
 cd ~/tools/ && wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux && chmod +x findomain-linux && mv findomain-linux /usr/bin/findomain 2> /dev/null
+cd && git clone https://github.com/devanshbatham/paramspider && cd paramspider && sudo pip install .
 echo "Installing Wordlists & Payloads"
 cd ~/wordlists/ && wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/big.txt
 cd ~/wordlists/ && wget https://gist.githubusercontent.com/Lopseg/33106eb13372a72a31154e0bbab2d2b3/raw/a79331799a70d0ae0ea906f2b143996d85f71de5/dicc.txt
@@ -51,17 +52,16 @@ go get github.com/michenriksen/aquatone 2> /dev/null
 go get -u github.com/tomnomnom/assetfinder 2> /dev/null
 GO111MODULE=on go get -u -v github.com/bp0lr/gauplus 2> /dev/null
 GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx 2> /dev/null
-go get -v github.com/OWASP/Amass/v3/... 2> /dev/null
 go get github.com/tomnomnom/waybackurls 2> /dev/null
 go get github.com/Emoe/kxss 2> /dev/null
 go get github.com/haccer/subjack 2> /dev/null
 go get -u github.com/tomnomnom/qsreplace 2> /dev/null
+go install github.com/projectdiscovery/katana/cmd/katana@latest && cd /go/bin/ && mv katana /usr/bin 2> /dev/null
 cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/dnsx.git; cd dnsx/cmd/dnsx; go build; mv dnsx /usr/bin/ &> /dev/null 
 cd ~/tools/temp/ && git clone https://github.com/hahwul/dalfox && cd dalfox && go install 2> /dev/null
 cd ~/tools/temp/ && git clone https://github.com/dwisiswant0/crlfuzz && cd crlfuzz/cmd/crlfuzz && go build . && sudo mv crlfuzz /usr/bin/ 2> /dev/null
 cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/nuclei.git && cd nuclei/v2/cmd/nuclei && go build && mv nuclei /usr/bin/ 2> /dev/null
 cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/subfinder.git && cd subfinder/v2/cmd/subfinder && go build && mv subfinder /usr/bin/ 2> /dev/null
-cd ~/tools/temp/ && wget https://github.com/OWASP/Amass/releases/download/v3.13.4/amass_linux_i386.zip && unzip amass_linux_i386.zip && cd amass_linux_i386 && mv amass /usr/bin/ 2> /dev/null
 cd
 cp -r ~/go/src/github.com/tomnomnom/gf/examples ~/.gf/
 echo 'source ~/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
